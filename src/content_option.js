436a85fa45aa25d45f0143d2608416b382b2d644
Adithyanshibu <steyp077@gmail.com>
2025-05-13 14:05:56 +0530

    portfolio

---
+++ src/content_option.js
@@ -0,0 +1,165 @@
+const logotext = "Portfolio";
+const meta = {
+    title: "Adithyan",
+    description: "I’m Adithyan Shibu UI/UX ui/ux developeing students",
+};
+
+const introdata = {
+    title: "I’m Adithyan Shibu",
+    animated: {
+        first: "I'm A UI/UX Student",
+        second: "Exploring studies",
+        third: "Building skills",
+    },
+    description: "I am a UI/UX engineering student specializing in the developing and optimization of user-centric, functional, and intuitive useer experiences.",
+    your_img_url: "/assets/images/img1.jpg",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "Hi, I'm a passionate UI/UX design student, currently building my skills and exploring the world of user-centered design. I'm on a journey of continuous learning, driven by curiosity and creativity to craft meaningful digital experiences.",
+};
+const worktimeline = [{
+        jobtitle: "SSLC",
+        where: "SN Public School, Chenkottukonam, Thiruvananthapuram",
+        date: "2022",
+    },
+    {
+        jobtitle: "+2(Higher Secondary)",
+        where: "SNGHSS Chempazhanthy - Commerce Computer",
+        date: "2022-2024",
+    },
+    {
+        jobtitle: "College",
+        where: "SN University - BCA",
+        date: "Ongoing",
+    },
+];
+
+const skills = [{
+        name: "UI Engineer",
+        value: 80,
+    },
+    {
+        name: "Backend Developer",
+        value: 90,
+    },
+    {
+        name: "DevOps Engineer",
+        value: 100,
+    },
+    {
+        name: "Web Application Develope",
+        value: 80,
+    },
+    {
+        name: "Mobile Application Developer",
+        value: 65,
+    },
+];
+
+const services = [{
+        title: "UI Engineer",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Backend Developer",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "shibuadithan@gmail.com",
+    YOUR_FONE: "+91 7736829802",
+    description: "Feel free to get in touch with me. I am always open to discussing new projects, creative ideas, or opportunities to be part of your vision.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/Adithyanshibu",
+    instagram: "https://www.instagram.com/__dhe_zerox__/",
+    linkedin: "https://www.linkedin.com/in/adithan-shibu-5a75a833a/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
